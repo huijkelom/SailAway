54a501e1cde74a0fab3571215cd2088c67322ac7
Sven Janssen <svenjanssen12321@gmail.com>
2024-04-18 14:37:35 +0200

    Made a build

---
+++ Assets/Editor/PostBuildVersionFileInserter.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+public class PostBuildVersionFileInserter : IPostprocessBuildWithReport
+{
+    const string VersionFileName = "BuildVersion.txt";//this is also hard set in PreBuildVersionSetter and MenuController.GetVersion()
+    const string VersionFileNameInDeploy = "BuildVersionGame.txt"; //this is also hard set in de smartwall central controller application
+
+    public int callbackOrder { get { return 0; } }
+    [PostProcessBuild]
+    public void OnPostprocessBuild(BuildReport report)
+    {
+        string srcPath = Path.Combine(Application.streamingAssetsPath, VersionFileName);
+        string destPath = Path.Combine(Path.GetDirectoryName(report.summary.outputPath), VersionFileNameInDeploy);
+        File.Copy(srcPath, destPath,true);
+    }
+}
---
+++ Assets/Editor/PreBuildVersionSetter.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+public class PreBuildVersionSetter : IPreprocessBuildWithReport
+{
+    const string VersionFileName = "BuildVersion.txt"; //this is also hard set in PostBuildVersionFileInserter and MenuController.GetVersion()
+    const string PackageVersion = "P1";
+
+    public int callbackOrder { get { return 0; } }
+    public void OnPreprocessBuild(BuildReport report)
+    {
+        string path = Path.Combine(Application.streamingAssetsPath, VersionFileName);
+        int previusVersion = 0;
+        if (File.Exists(path))
+        {
+            string versionSavedInFile = File.ReadAllText(path);
+            previusVersion = int.Parse(versionSavedInFile.Split('.')[1]);
+        }
+        try
+        {
+            previusVersion++;
+            WriteVersionFile(PackageVersion, previusVersion, path);
+        }
+        catch (IOException iOEx)
+        {
+            Debug.LogError("Failed to update build version file");
+        }
+    }
+
+    private void WriteVersionFile(string packageVersion, int versionNumber, string path)
+    {
+        if (path.Equals(string.Empty)) {  }
+        using (StreamWriter sr = File.CreateText(path))
+        {
+            sr.Write(packageVersion + "." + versionNumber.ToString());
+        }
+    }
+}
